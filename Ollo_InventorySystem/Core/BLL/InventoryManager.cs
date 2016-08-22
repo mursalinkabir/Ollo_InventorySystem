93e28825aa0b3b58baac852afdb6301ec5833170
mursalin_kabir <mail2mursalin@gmail.com>
2016-08-22 16:09:09 +0600

    2nd commit

---
+++ Ollo_InventorySystem/Core/BLL/InventoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ollo_InventorySystem.Core.BLL
+{
+    public class InventoryManager
+    {
+    }
+}
