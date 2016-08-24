d1464e3b9db25404085bfe775c5f1cca4a3b4491
mursalin_kabir <mail2mursalin@gmail.com>
2016-08-24 14:00:30 +0600

    Import Working with XLXS Format
    Proper Format Follow korle Import is working

--- Ollo_InventorySystem/Core/BLL/ImportManager.cs
+++ Ollo_InventorySystem/Core/BLL/ImportManager.cs
@@ -14,7 +14,7 @@ namespace Ollo_InventorySystem.Core.BLL
         ImportGateway importGateway = new ImportGateway();
         public bool ReadExcelFile(string fileFullPath)
         {
-            string LongQueryString = "INSERT INTO LteRouter ( ItemCode, ItemDescription, Batch , Tag , Serial , Mac , Imei , CheckKey ) VALUES ";
+            string LongQueryString = "INSERT INTO LteRouter ( ItemCode, ItemDescription, Batch , Tag , Serial , Mac , Imei ) VALUES ";
             
             int isDataAdded = 0;
             if (fileFullPath != "")
@@ -28,28 +28,27 @@ namespace Ollo_InventorySystem.Core.BLL
                 DataSet result = excelReader.AsDataSet();
                 foreach (DataRow row in result.Tables["Sheet1"].Rows)
                 {
-                    string Item_code, Item_description,Batch,Tag_no,Serial_no,Mac_id,Imei_code,Check;
-                    Item_code = Item_description = Batch = Tag_no = Serial_no = Mac_id = Imei_code = Check = "";
-                    if ((!string.IsNullOrEmpty(row["Item-Code"].ToString())) & (!string.IsNullOrEmpty(row["Item-Description"].ToString())) & (!string.IsNullOrEmpty(row["Batch"].ToString())) & (!string.IsNullOrEmpty(row["Tag-No"].ToString())) & (!string.IsNullOrEmpty(row["Serial-No"].ToString())) & (!string.IsNullOrEmpty(row["MAC-ID"].ToString())) & (!string.IsNullOrEmpty(row["IMEI-Code"].ToString())) & (!string.IsNullOrEmpty(row["Check"].ToString())))
+                    string Item_code, Item_description,Batch,Tag_no,Serial_no,Mac_id,Imei_code;
+                    Item_code = Item_description = Batch = Tag_no = Serial_no = Mac_id = Imei_code = "";
+                    if ((!string.IsNullOrEmpty(row["Item-Code"].ToString())) & (!string.IsNullOrEmpty(row["Item-Description"].ToString())) & (!string.IsNullOrEmpty(row["Batch"].ToString())) & (!string.IsNullOrEmpty(row["Tag-No"].ToString())) & (!string.IsNullOrEmpty(row["Serial-No"].ToString())) & (!string.IsNullOrEmpty(row["MAC-ID"].ToString())) & (!string.IsNullOrEmpty(row["IMEI-Code"].ToString())) )
                     {
-                        Item_code = row["Item-Code"].ToString();
-                        Item_description = row["Item-Description"].ToString();
-                        Batch = row["Batch"].ToString();
-                        Tag_no = row["Tag-No"].ToString();
-                        Serial_no = row["Serial-No"].ToString();
-                        Mac_id = row["MAC-ID"].ToString();
-                        Imei_code = row["IMEI-Code"].ToString();
-                        Check = row["Check"].ToString();
+                        Item_code = "'" + row["Item-Code"].ToString() + "'";
+                        Item_description = "'" + row["Item-Description"].ToString() +"'";
+                        Batch = "'" + row["Batch"].ToString() + "'";
+                        Tag_no = "'" + row["Tag-No"].ToString() + "'";
+                        Serial_no = "'" + row["Serial-No"].ToString() + "'";
+                        Mac_id = "'" + row["MAC-ID"].ToString() + "'";
+                        Imei_code = "'" + row["IMEI-Code"].ToString() + "'";
+                        
                     }
 
 
-                    LongQueryString = LongQueryString + "( " + Item_code + ", " + Item_description +
-                                      " ," + Batch +
-                                      " ," + Tag_no +
-                                      " ," + Serial_no +
-                                      " ," + Mac_id +
-                                      " ," + Imei_code +
-                                      " ," + Check +
+                    LongQueryString = LongQueryString + "( " + Item_code + "," + Item_description +
+                                      "," + Batch +
+                                      "," + Tag_no +
+                                      "," + Serial_no +
+                                      "," + Mac_id +
+                                      "," + Imei_code +
                                       " ),";
 
 
